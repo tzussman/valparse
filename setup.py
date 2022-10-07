8327e7a742dadea20f12797e9fcfc9ec33f065f2
Tal Zussman <tz2294@columbia.edu>
2022-10-07 15:32:08 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="valparse",
+    version="0.1",
+    author="valparse contributors",
+    author_email="tz2294@columbia.edu",
+    description="Package to parse Valgrind XML files.",
+    url="https://github.com/tzussman/valparse",
+    python_requires=">=3.7",
+    packages=setuptools.find_packages(),
+)
