33ed4830db3521cb1cdcc630c8348dac22aa1b6f
Tal Zussman <tz2294@columbia.edu>
2023-04-22 01:42:55 -0400

    Add a suppresion test

--- valparse/tests/test_parser.py
+++ valparse/tests/test_parser.py
@@ -1,14 +1,16 @@
 '''Integration tests for Valparse.'''
 
+import filecmp
 import signal
 from pathlib import Path
 
+import valparse
 from valparse import Parser
 from valparse.vgerror import ValgrindErrorKind
 
 
 def test_Parser():
-    """Create an instance of a Parser using data/bad.xml and verify the output"""
+    """Create an instance of a Parser using data/bad-test.xml and verify the output"""
 
     filename = Path(__file__).resolve().parent / 'data/bad-test.xml'
 
@@ -35,3 +37,33 @@ def test_Parser():
     assert valfile.pid == 10220
     assert valfile.ppid == 8125
     assert valfile.tool == 'memcheck'
+
+
+def test_suppression_dump():
+    """Create an instance of a Parser using data/bad.xml and dump suppresions"""
+    
+    filename = Path(__file__).resolve().parent / 'data/bad-test.xml'
+
+    try:
+        valfile = Parser(filename)
+    except FileNotFoundError:
+        print(f"File '{filename}' not found. The tested executable might have failed...", "red")
+        exit(1)
+    except Exception as e:
+        print(f"Exception raised: {e}. The tested executable might have failed...", "red")
+        exit(1)
+
+    assert valfile.hasErrors() is True
+    assert valfile.hasLeaks() is False
+    assert valfile.uniqueErrCount() == 2
+    assert valfile.uniqueLeakCount() == 0
+    assert valfile.totalBytesLeaked() == 0
+    assert valfile.hasFatalSignal() is True
+
+    test_supp_file = Path(__file__).resolve().parent / 'data/test.supp'
+    supp_file = Path(__file__).resolve().parent / 'data/bad.supp'
+
+    print(valfile.suppressions)
+
+    valparse.parser.dumpSuppressions(test_supp_file, [('definite-leak-ignore', valfile.suppressions[0])])
+    assert filecmp.cmp(supp_file, test_supp_file, shallow=False)
